14d4781660e55e2b216d1a218dec654e7eefec95
k.miyashita <k.miyashita@gxp.co.jp>
2024-01-23 14:33:19 +0900

    バーコードとトラッキング機能追加

--- qrfront/app.vue
+++ qrfront/app.vue
@@ -3,6 +3,30 @@ import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader';
 const value = ref();
 const isQrcode = ref(false);
 
+const barcodeTypes = [
+  'aztec',
+  'code_128',
+  'code_39',
+  'code_93',
+  'codabar',
+  'databar',
+  'databar_expanded',
+  'data_matrix',
+  'dx_film_edge',
+  'ean_13',
+  'ean_8',
+  'itf',
+  'maxi_code',
+  'micro_qr_code',
+  'pdf417',
+  'qr_code',
+  'rm_qr_code',
+  'upc_a',
+  'upc_e',
+  'linear_codes',
+  'matrix_codes',
+];
+
 const onDetect = (content) => {
   value.value = content[0].rawValue;
 };
@@ -10,6 +34,23 @@ const onDetect = (content) => {
 const handleButton = (content) => {
   isQrcode.value = !isQrcode.value;
 };
+
+function paintOutline(detectedCodes, ctx) {
+  for (const detectedCode of detectedCodes) {
+    const [firstPoint, ...otherPoints] = detectedCode.cornerPoints;
+
+    ctx.strokeStyle = 'red';
+
+    ctx.beginPath();
+    ctx.moveTo(firstPoint.x, firstPoint.y);
+    for (const { x, y } of otherPoints) {
+      ctx.lineTo(x, y);
+    }
+    ctx.lineTo(firstPoint.x, firstPoint.y);
+    ctx.closePath();
+    ctx.stroke();
+  }
+}
 </script>
 <template>
   <div class="app">
@@ -17,14 +58,12 @@ const handleButton = (content) => {
     <h2>QRcodeの値：{{ value }}</h2>
     <button @click="handleButton">QRcode読み取り</button>
     <div v-if="isQrcode" class="qr">
-      <qrcode-stream @detect="onDetect"></qrcode-stream>
+      <qrcode-stream
+        @detect="onDetect"
+        :track="paintOutline"
+        :formats="barcodeTypes"
+      ></qrcode-stream>
     </div>
-    <!-- <div>
-      <qrcode-drop-zone @detect="onDetect" class="drop"
-        >ドラッグ&ドロップ</qrcode-drop-zone
-      >
-    </div> -->
-
     <div class="capture">
       <qrcode-capture @detect="onDetect"></qrcode-capture>
     </div>
